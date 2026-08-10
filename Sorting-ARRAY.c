cbbc64895752aa2fb0b27dc0e41bdf58adbf21ef
Kaneki <centipede.ghoul240@gmail.com>
2026-08-11 00:18:59 +0530

    Refactor sorting algorithm for improved clarity and correctness

--- Sorting-ARRAY.c
+++ Sorting-ARRAY.c
@@ -2,21 +2,25 @@
 #include<stdio.h>
 void main()
 {
-    int arr[10],i,size,small,temp;
+    int arr[10],i,j,size,small,temp;
     printf("Enter the array size: ");
      scanf("%d",&size);
     printf("Enter the Array: ");
     for(i=0;i<size;i++)
         scanf("%d",&arr[i]);
-    small = arr[0];
-    for(i=1;i<size;i++)
+    for(i = 0; i < size - 1; i++)
     {
-        if(small>arr[i])
+        small = i;
+
+        for(j = i + 1; j < size; j++)
         {
-            temp = arr[i];
-            arr[i] = small;
-            small = temp;
+            if(arr[j] < arr[small])
+                small = j;
         }
+
+        temp = arr[i];
+        arr[i] = arr[small];
+        arr[small] = temp;
     }
     printf("The sorted Array in ascending: ");
     for(i=0;i<size;i++)
