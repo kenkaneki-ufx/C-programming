9fcc418b3949b482b3d7e375c216f3033ac96308
Kaneki <centipede.ghoul240@gmail.com>
2026-08-13 19:46:54 +0530

    Remove _automation backup folder (not wanted)

--- Daily-Tasks/_automation/push-to-github.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\SERVER\Desktop\VS Code\C" || exit /b 1
-git add "Daily-Tasks" >> "%~dp0_git.log" 2>&1 || exit /b 1
-git commit -m "Daily task: %~n1" >> "%~dp0_git.log" 2>&1 || exit /b 1
-git push >> "%~dp0_git.log" 2>&1
