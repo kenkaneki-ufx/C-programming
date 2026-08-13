105d7f36b58508e089d9b2a11599ce5f7fe1b6f1
Kaneki <centipede.ghoul240@gmail.com>
2026-08-13 05:46:02 +0530

    Refactor smallest and largest functions for clarity and correctness

--- small_element-ARRAY.c
+++ small_element-ARRAY.c
@@ -1,21 +1,39 @@
 // WAP to print the position of the smallest number of n numbers using array
 #include<stdio.h>
-void main()
+void smallest(int *arr,int n)
 {
-    int arr[10],i,n,pos=0,size,smallest;
-    printf("Enter the array size: ");
-     scanf("%d",&size);
-    printf("Enter the Array:\n");
-    for(i=0;i<size;i++)
-        scanf("%d",&arr[i]);
-    smallest = arr[pos];
-    for(i=1;i<size;i++)
+    int i,smallest = arr[0];
+    for(i=1;i<n;i++)
     {
-        if(arr[i]<smallest)
+        if(smallest > arr[i])
         {
             smallest = arr[i];
-            pos = i;
+            break;
         }
     }
-    printf("The smallest element %d is at %d position",smallest,pos);
+    printf("\nThe smallest element %d is at %d position",smallest,i);
+}
+void largest(int *arr,int n)
+{
+    int i,largest = arr[0];
+    for(i=0;i<n;i++)
+    {
+        if (largest < arr[i])
+        largest = arr[i];
+        break;
+    }
+    printf("\nThe largest element %d is at %d position",largest,i);
+}
+
+int main()
+{
+    int arr[10],size;
+    printf("Enter the array size: ");
+     scanf("%d",&size);
+    printf("Enter the Array: ");
+    for(int i=0;i<size;i++)
+        scanf("%d",&arr[i]);
+    smallest(arr,size);
+    largest(arr,size);    
+    return 0;
 }
