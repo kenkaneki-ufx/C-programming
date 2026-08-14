4902891287273abea5849bc63bdcde2667f93f77
Kaneki <centipede.ghoul240@gmail.com>
2026-08-14 23:49:55 +0530

    Daily task: Daily task: Day-01_Swap

--- Daily-Tasks/Day-01_Swap.c
+++ Daily-Tasks/Day-01_Swap.c
@@ -1,29 +1,26 @@
 /* ============================================================
-   📅 DAILY CODING TASK — DAY 1 (Pointers)
+   📅 DAILY CODING TASK — DAY 1 (Swap - beginner friendly)
    ============================================================
 
-   ❓ QUESTION: Can you swap two numbers using a function
-   that receives the values instead of pointers?
+   ❓ QUESTION: Can you swap two numbers using a function that receives the values?
 
    TASK:
    1. Declare two variables: a = 5, b = 10
    2. Try swapping them with a function that takes VALUES
-   3. Then swap them with a function that takes POINTERS
-   4. Print a and b after each attempt
-
+   4. Print a and b before & after swap
    ============================================================ */
-
 #include <stdio.h>
-int swap(int *a,int *b)
+int swap(int a,int b)
 {
-    int c = *a;
-    *a = *b;
-    *b = c;
+    int c = a;
+    a = b;
+    b = c;
+    printf("\nAfter Swap\na:%d\nb:%d",a,b);
 }
 void main() 
 {
     int a = 5, b = 10;
-    swap(&a,&b);
-    printf("a:%d\nb:%d",a,b);
+    printf("\nBefore Swap\na:%d\nb:%d",a,b);
+    swap(a,b);
 
 }
